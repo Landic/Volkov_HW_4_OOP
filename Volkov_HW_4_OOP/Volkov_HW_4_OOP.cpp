57b38fd4978d521aea0fc20865ec1ba0df7e9e3b
Danya <danil.volkov.235@gmail.com>
2022-09-06 19:35:11 +0300

    Добавил необходимые конструкторы

--- Volkov_HW_4_OOP/Volkov_HW_4_OOP.cpp
+++ Volkov_HW_4_OOP/Volkov_HW_4_OOP.cpp
@@ -5,6 +5,14 @@ using namespace std;
 class Point {
 	int x, y;
 public:
+	Point() {
+		x = 0;
+		y = 0;
+	}
+	Point(int value_x, int value_y) {
+		x = value_x;
+		y = value_y;
+	}
 };
 
 int main() {
